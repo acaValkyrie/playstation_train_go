d719c04e7586f6127355de21b6995f394006dac6
acaValkyrie <acavalkyrie@gmail.com>
2023-11-23 00:26:15 +0900

    PとBの値に応じてspeedを出力するように変更

--- src/main.cpp
+++ src/main.cpp
@@ -7,27 +7,15 @@
 #define attPin 11       //アテンションピン番号
 #define clockPin 10     //クロックピン番号
 
-const int EB = 2;         //非常リレーピン番号
-const int Start = 3;      //スタートTRピン番号
-const int sankaku = 4;    //三角TRピン番号
-const int maru = 5;       //丸TRピン番号
-const int batsu = 6;      //バツTRピン番号
-const int shikaku = 7;    //三角TRピン番号
-const int wt = 3000;     //モード選択長押し時間指定
+#define pwmOutPin 3
 
 Psx MassController;
-                                                                                     //デジポテ情報
-const int OneHan1[] = {127,173,192,211,232,255,89,79,68,57,45,34,23,0,0};            //ワンハンドル位置
-const int TwoHan1[] = {127,173,192,211,232,255,170,181,192,203,213,224,235,255,255}; //ツーハンドル位置
-const int TwoHan2[] = {127,82,67,30,0,0,178,202,227,255,255,255,255,255,255};        //ツーハンドル(103系)位置
-const int SousaYT[] = {0,250,1000,0,1000,250};                                       //以下選択モード用
-const int SousaYS[] = {127,0,0,127,255,255};
-const int SousaXT[] = {0,250,350,500,700,800,1000,0,1000,800,700,500,350,250,0};
-const int SousaXS[] = {127,0,0,0,0,0,0,127,255,255,255,255,255,255,127};
 
 unsigned int data = 0;
-int P,B,S,Pbit,Bbit,BTNbit,i,ii,HSW1val,Mode,ModeST,m,t;
+int P,B,S,Pbit,Bbit;
 unsigned int Handle[]={15,2062,14,2059,11,2058,13,7,5,14,12,6,4,11,9,3,1,10,8,2,0};  //ハンドルビット判別
+
+float speed;
 
 void setup(){
   MassController = Psx();
@@ -37,18 +25,20 @@ void setup(){
 
   P = 0;
   B = 0;
+
+  pinMode(pwmOutPin, OUTPUT);
+  speed = 0;
 }
 void loop(){
   data = MassController.read();
   Pbit = data & 2063; //bit処理
   Bbit = data >> 12;
-  i = 2;
 
   if((Pbit == (int)Handle[0]) && (Bbit == (int)Handle[6])){  //ニュートラル判定
     P = 0;
     B = 0;
   }else{                                                     //マスコン・ブレーキ判定
-    for (i = 0; i <= 22; i++) {
+    for (int i = 0; i <= 22; i++) {
       if(i <= 5){
         if(Pbit == (int)Handle[i]){
           P = i;
@@ -69,5 +59,14 @@ void loop(){
   Serial.print(Bbit);             //ブレーキビット表示
   Serial.print(", B:");
   Serial.print(B);                //ブレーキ表示
-  Serial.println();
+
+  speed += P * 0.1 - B * 0.1;
+  if(speed > 255){
+    speed = 255;
+  }else if(speed < 0){
+    speed = 0;
+  }
+  Serial.print(", speed:");
+  Serial.println(speed);          //速度表示
+  analogWrite(pwmOutPin, (int)speed);
 }
