a3ba8a96735ff93c210b49ad99d8471c53e8c532
acaValkyrie <acavalkyrie@gmail.com>
2023-11-23 02:10:30 +0900

    add comment

--- src/main.cpp
+++ src/main.cpp
@@ -8,13 +8,18 @@ void setup(){
 }
 
 void loop(){
+  // マスコンの現在のレバーの位置を取得する。
   updateBrakeAccell();
 
+  // マスコンの現在のレバーの位置に応じて、モーターの回転速度の変化量を決定する。
   updateSpeed();
+  // モーターの回転速度の変化量に応じて、モーターの回転速度を変化させる。
   int speed = (int)getSpeed();
 
+  // モーターの回転速度を変化させる。
   analogWrite(pwmOutPin, (int)speed);
-  
+
+  // デバッグ用のプリント
   printMassconPropaties();
   printSpeed();
 }
